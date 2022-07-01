541934850c30347411065866ea446ed82c75bc71
Tomas Buroci <108458651+TomasBuroci@users.noreply.github.com>
2022-07-01 19:03:04 +0200

    Update streamlit_app.py
    added function for snowflake table

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,14 +52,18 @@ import snowflake.connector
 #dont add anything
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
-streamlit.text("The fruit load list:")
-streamlit.text(my_data_row)
+#snowflake functions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchone()
+   
+#add button to load fruit
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_row)
+   
 #fruityvice API response add fruit
 fruit_choice_2 = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ' + fruit_choice_2)
